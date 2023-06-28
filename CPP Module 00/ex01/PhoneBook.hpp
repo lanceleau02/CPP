d07c75544de73bbf3d8a2ece463265ac9b8c67e8
Lancelot Prieure <laprieur@2F7.42angouleme.fr>
2023-06-28 16:54:10 +0200

    pas grand chosr

--- CPP Module 00/ex01/PhoneBook.hpp
+++ CPP Module 00/ex01/PhoneBook.hpp
@@ -6,10 +6,17 @@
 /*   By: laprieur <laprieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 18:17:36 by laprieur          #+#    #+#             */
-/*   Updated: 2023/06/27 18:24:56 by laprieur         ###   ########.fr       */
+/*   Updated: 2023/06/28 14:51:10 by laprieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
 class	PhoneBook {
-	
-}
+	public:
+		int	contact[8];
+		
+};
+
+#endif
