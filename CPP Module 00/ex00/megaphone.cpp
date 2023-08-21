a9e27f94cbb0e05d75dc1ebcd75cfdbc7f81767c
Lancelot Prieure <laprieur@1H4.42angouleme.fr>
2023-08-21 12:59:32 +0200

    Better

--- CPP Module 00/ex00/megaphone.cpp
+++ CPP Module 00/ex00/megaphone.cpp
@@ -6,22 +6,24 @@
 /*   By: laprieur <laprieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 15:14:48 by laprieur          #+#    #+#             */
-/*   Updated: 2023/08/04 10:52:01 by laprieur         ###   ########.fr       */
+/*   Updated: 2023/08/21 12:52:55 by laprieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
 void	megaphone(int argc, char **argv) {
+	std::string str;
+
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else {
 		for (int i = 1; argv[i]; i++) {
 			for (int j = 0; argv[i][j]; j++) {
-				std::cout << static_cast<char>(std::toupper(argv[i][j]));
+				str.push_back(std::toupper(argv[i][j]));
 			}
 		}
-		std::cout << std::endl;
+		std::cout << str << std::endl;
 	}
 }
 
