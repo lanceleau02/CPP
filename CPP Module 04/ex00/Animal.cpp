259c4616ffe9ccbbc4c977917bdd5a59656b42dc
Lancelot Prieure <laprieur@1C5.42angouleme.fr>
2023-09-08 16:53:43 +0200

    Some adds in CPP Module 04

---
+++ CPP Module 04/ex00/Animal.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: laprieur <laprieur@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/08 16:36:43 by laprieur          #+#    #+#             */
+/*   Updated: 2023/09/08 16:52:44 by laprieur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+
+Animal::Animal() : _type("Animal") {
+	std::cout << GREEN << "Animal class Default constructor called!" << NONE << std::endl;
+}
+
+Animal::Animal(const Animal& source) {
+	std::cout << GREEN << "Animal class Copy constructor called!" << NONE << std::endl;
+	*this = source;
+}
+
+Animal&	Animal::operator=(const Animal& source) {
+	std::cout << GREEN << "Animal class Assignment operator called!" << NONE << std::endl;
+	_type = source._type;
+	return *this;
+}
+
+Animal::~Animal() {
+	std::cout << RED << "Animal class Default destructor called!" << NONE << std::endl;
+}
+
+void	Animal::makeSound(void) {
+	if (_type == "Dog")
+		std::cout << BLUE << "Waf! Waf!" << NONE << std::endl;
+	else if (_type == "Cat")
+		std::cout << BLUE << "Meow!" << NONE << std::endl;
+}
--- CPP Module 04/ex00/Animal.hpp
+++ CPP Module 04/ex00/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: laprieur <laprieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 15:22:46 by laprieur          #+#    #+#             */
-/*   Updated: 2023/09/07 15:23:45 by laprieur         ###   ########.fr       */
+/*   Updated: 2023/09/08 16:51:38 by laprieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,24 @@
 # define ANIMAL_HPP
 
 #include <string>
+#include <iostream>
+
+#define RED		"\033[31m"
+#define GREEN	"\033[32m"
+#define BLUE	"\033[34m"
+#define NONE	"\033[0m"
 
 class Animal {
 	protected:
-		std::string	type;
+		std::string	_type;
 
-	
+	public:
+		Animal();
+		Animal(const Animal& source);
+		Animal&	operator=(const Animal& source);
+		virtual ~Animal();
+		
+		virtual void	makeSound(void);
 };
 
 #endif
