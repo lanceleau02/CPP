8cf5ffc651337cb0ec058ace5a926496b977e879
Lancelot Prieure <laprieur@2F5.42angouleme.fr>
2023-10-19 11:31:55 +0200

    CPP Module 06: finished but need to be verified

--- CPP Module 06/ex01/main.cpp
+++ CPP Module 06/ex01/main.cpp
@@ -6,11 +6,14 @@
 /*   By: laprieur <laprieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 13:44:27 by laprieur          #+#    #+#             */
-/*   Updated: 2023/10/18 14:29:04 by laprieur         ###   ########.fr       */
+/*   Updated: 2023/10/19 11:30:57 by laprieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
+
+#define RED		"\033[31m"
+#define GREEN	"\033[32m"
 
 int	main(void) {
 	Data		obj;
@@ -19,7 +22,8 @@ int	main(void) {
 	Data*		deserialized = Serializer::deserialize(serialized);
 
 	if (ptr == deserialized)
-		std::cout << "Success!" << std::endl;
+		std::cout << GREEN << "Success!" << std::endl;
 	else
-		std::cout << "Fail!" << std::endl;
+		std::cout << RED << "Fail!" << std::endl;
+	return (0);
 }
--- CPP Module 06/ex02/main.cpp
+++ CPP Module 06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: laprieur <laprieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 15:01:37 by laprieur          #+#    #+#             */
-/*   Updated: 2023/10/18 16:40:29 by laprieur         ###   ########.fr       */
+/*   Updated: 2023/10/19 11:28:02 by laprieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,10 @@
 #include <cstdlib>
 #include <ctime>
 
+#define YELLOW	"\033[33m"
+#define BLUE	"\033[34m"
+#define NONE	"\033[0m"
+
 Base*	generate(void) {
 	switch (std::rand() % 3) {
 		case 0:
@@ -30,14 +34,68 @@ Base*	generate(void) {
 	}
 }
 
-/* void	identify(Base* p) {
+void	identify(Base* p) {
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+}
 
-} */
+void	identify(Base& p) {
+	try {
+		A& a = dynamic_cast<A&>(p);
+		(void)a;
+		std::cout << "A" << std::endl;
+	}
+	catch (const std::exception& e) {}
+	try { 
+		B& b = dynamic_cast<B&>(p);
+		(void)b;
+		std::cout << "B" << std::endl;
+	}
+	catch (const std::exception& e) {}
+	try {
+		C& c = dynamic_cast<C&>(p);
+		(void)c;
+		std::cout << "C" << std::endl;
+	}
+	catch (const std::exception& e) {}
+}
 
 int	main(void) {
 	std::srand(std::time(NULL));
 	
-	Base*	a = generate();
+	Base*	obj1 = generate();
+	Base*	obj2 = generate();
+	Base*	obj3 = generate();
+	Base*	obj4 = new Base();
 	
-	std::cout << &a << std::endl;
+	std::cout << YELLOW << "+--------+ identify(*) FUNCTION TESTS +--------+" << NONE << std::endl;
+	std::cout << std::endl;
+	{
+		std::cout << BLUE << "The type of obj1 is: " << NONE;
+		identify(obj1);
+		std::cout << BLUE << "The type of obj2 is: " << NONE;
+		identify(obj2);
+		std::cout << BLUE << "The type of obj3 is: " << NONE;
+		identify(obj3);
+		std::cout << BLUE << "The type of obj4 is: " << NONE;
+		identify(obj4);
+	}
+	std::cout << std::endl << std::endl;
+	std::cout << YELLOW << "+--------+ identify(&) FUNCTION TESTS +--------+" << NONE << std::endl;
+	std::cout << std::endl;
+	{
+		std::cout << BLUE << "The type of obj1 is: " << NONE;
+		identify(*obj1);
+		std::cout << BLUE << "The type of obj2 is: " << NONE;
+		identify(*obj2);
+		std::cout << BLUE << "The type of obj3 is: " << NONE;
+		identify(*obj3);
+		std::cout << BLUE << "The type of obj4 is: " << NONE;
+		identify(*obj4);
+	}
+	std::cout << std::endl;
 }
