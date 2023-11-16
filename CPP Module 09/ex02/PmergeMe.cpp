4ef609fb6da3d32fa9bb6ec6ac4047540c24fb28
Lancelot Prieure <laprieur@3E6.42angouleme.fr>
2023-11-16 16:37:12 +0100

    Tom m'a donné les cramptés

--- CPP Module 09/ex02/PmergeMe.cpp
+++ CPP Module 09/ex02/PmergeMe.cpp
@@ -6,9 +6,38 @@
 /*   By: laprieur <laprieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 11:40:55 by laprieur          #+#    #+#             */
-/*   Updated: 2023/11/14 11:41:29 by laprieur         ###   ########.fr       */
+/*   Updated: 2023/11/16 14:50:21 by laprieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
+std::vector<int>	PmergeMe(std::vector<int> X) {
+	if (X.size() == 1)
+		return X;
+
+	std::vector<s_pair>	newX;
+	std::vector<int>	toRepeat;
+	std::vector<int>	S;
+	s_pair				tmp;
+	int i = 0;
+	
+	for (std::vector<int>::iterator it = X.begin(); it != X.end(); it += 2; i++) {
+		if (*it > *(it + 1)) {
+			tmp.min = *(it + 1);
+			tmp.max = *it;
+		} else {
+			tmp.min = *it;
+			tmp.max = *(it + 1);
+		}
+		newX.push_back(tmp);
+		toRepeat.push_back(tmp.max);
+		S = PmergeMe(toRepeat);
+	}
+	
+	std::vector<int> copyS(S);
+	
+	for (std::vector<int>::iterator it = S.begin(); it != S.end(); it++) {
+		copyS.insert(copyS.begin() + pos, newX[pos2].min);
+	}
+}
--- CPP Module 09/ex02/PmergeMe.hpp
+++ CPP Module 09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: laprieur <laprieur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 11:40:53 by laprieur          #+#    #+#             */
-/*   Updated: 2023/11/14 11:48:07 by laprieur         ###   ########.fr       */
+/*   Updated: 2023/11/16 14:39:44 by laprieur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 
 #include <vector>
 
-std::vector<int>	
+typedef struct	s_pair {
+	unsigned int	min;
+	unsigned int	max;
+}	t_pair;
 
 #endif
